9f2d7d6b550e1a13e2b3c01f8b29bfe5c90989da
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-02-26 18:36:59 -0500

    Created sample registration page to in web app.

---
+++ views/layouts/main.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rutgers Flyers App</title>
+</head>
+<body>
+  <div class="row small-6 small-centered">
+    input[type='text' placeholder='']
+  </div>
+</body>
+</html>
