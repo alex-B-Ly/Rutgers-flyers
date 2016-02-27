8ebed1c9076419ceef91d4585730dec2f11baeb4
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-02-26 23:25:57 -0500

    Included functioning accordion on main page.

--- public/js/googlePlaces.js
+++ public/js/googlePlaces.js
@@ -1,23 +1,18 @@
 $(document).ready(function() {
 
-  alert('hello');
+  $(document).foundation();
+    // var googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+    // var apiKey = 'AIzaSyCoy7UBpNXFlBQKUGDtNz0ZhkgYC2cpPkg';
+    // var latLng = '40.4861111,-74.4522222';
+    // var radius = 10000;
+    // var type = 'restaurant';
+    // var apiEndPoint = googlePlacesURL + 'key=' + apiKey + '&location=' + latLng + '&radius=' + radius + '&type=' + type;
 
-  var elem = new Foundation.Accordion('accordion');
-
-  $('accordion').foundation('down');
-
-  // var googlePlacesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
-  // var apiKey = 'AIzaSyCoy7UBpNXFlBQKUGDtNz0ZhkgYC2cpPkg';
-  // var latLng = '40.4861111,-74.4522222';
-  // var radius = 10000;
-  // var type = 'restaurant';
-  // var apiEndPoint = googlePlacesURL + 'key=' + apiKey + '&location=' + latLng + '&radius=' + radius + '&type=' + type;
-
-  // console.log(apiEndPoint);
-  // $.getJSON(apiEndPoint, data, success) {
-  //   for (var restaurant in data) {
-  //     console.log(data[restaurant].results[0])
-  //   }
-  // }
+    // console.log(apiEndPoint);
+    // $.getJSON(apiEndPoint, data, success) {
+    //   for (var restaurant in data) {
+    //     console.log(data[restaurant].results[0])
+    //   }
+    // }
 
 });
