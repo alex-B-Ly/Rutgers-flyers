66a82cb29c78d5141ae4bf5629942f23ea08346c
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-02-28 00:57:07 -0500

    Successfully appended first column of dynamically added table w/ sample data.

--- public/js/googlePlaces.js
+++ public/js/googlePlaces.js
@@ -11,13 +11,31 @@ $(document).ready(function() {
     var apiEndPoint = googlePlacesURL + 'key=' + apiKey + '&location=' + latLng + '&radius=' + radius + '&type=' + type;
 
     //Reveal Data
-    $.getJSON(apiEndPoint, function(data) {
-        buildPanel(data);
-    });
+    // $.getJSON(apiEndPoint, function(data) {
+    //     buildPanel(data);
+    // });
 
-    // function buildPanel(data) {
+    buildPanel()
+    //Build Panel w/ Restaurant Data
+    function buildPanel(data) {
+        var panel = $('<div>').addClass('panel panel-default');
+        var panelBody = $('<div>').addClass('panel-body');
+        var row = $('<div>').addClass('row');
+        var col4 = $('<div>').addClass('col-xs-4');
+        var col12 = $('<div>').addClass('col-xs-12');
+        var image = $('<img>')
+            .addClass('img-thumbnail')
+            .attr('src', 'http://img2.10bestmedia.com/Images/Photos/261521/Restaurant---Cristal-Room--J-r-me-Mondi-re_54_990x660.jpg')
+            .attr('alt', '...');
+ 
+        var imgColumn = col4.append(image);
+        var imgRow = row.append(imgColumn);
+        var imgBody = panelBody.append(imgRow);
+        var imgContainer = panel.append(imgBody);
 
+        var panelContents = $('.main-column').append(imgContainer);
+        var newEntry = $('.main-row').append(panelContents);
 
-    // }
+    }
 
 });
