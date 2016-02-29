5a53f5bf1af8c20eb43fb6e7e2e6f197f8842f96
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-02-29 00:17:21 -0500

    Used callback to return data from ajax call and return it to the outer function.

--- public/js/googlePlaces.js
+++ public/js/googlePlaces.js
@@ -17,8 +17,6 @@ $(document).ready(function() {
     function buildPanel(data) {
         var panel = $('<div>').addClass('panel panel-default');
         var panelBody = $('<div>').addClass('panel-body');
-        // var col4 = $('<div>').addClass('col-xs-4');
-        var col12 = $('<div>').addClass('col-xs-12');
         var image = $('<img>')
             .addClass('img-thumbnail')
             .attr('src', 'http://img2.10bestmedia.com/Images/Photos/261521/Restaurant---Cristal-Room--J-r-me-Mondi-re_54_990x660.jpg')
@@ -30,8 +28,10 @@ $(document).ready(function() {
         //Dynamically add 2nd column of restaurant data
         var restaurant = data.results[1].name;
         var restaurantTitle = $('<h2>').html(restaurant);
-        var restaurantContainer = col12.append(restaurantTitle);
+        var restaurantContainer = $('<div>').addClass('col-xs-12').append(restaurantTitle);
         var restaurantRow = $('<div>').addClass('row').append(restaurantContainer);
+
+        //Append restaurant name and address
         var restaurantColumn = $('<div>').addClass('col-xs-4').append(restaurantRow);
 
 
@@ -40,23 +40,26 @@ $(document).ready(function() {
         var placeEndPoint = placeURL + placeId;
         console.log(placeEndPoint);
 
-        function getAddress(endpoint) {
+        function getAddress(endpoint, callback) {
             $.getJSON(endpoint, function (placeData) {
                 var streetNumber = placeData.result.address_components[0].short_name;
                 var streetName = placeData.result.address_components[1].short_name;
                 var city =  placeData.result.address_components[2].short_name;
                 var state = placeData.result.address_components[3].short_name;
-                var zipCode = placeData.result.address_components[4].short_name;
+                var zipCode = placeData.result.address_components[5].short_name;
                 var address = streetNumber + " " + streetName + " " + city + " " + state + " " + zipCode;
-                return address;
+                callback(address);
             });
-
-            return getAddress
         }
 
-        var returnedAddress = getAddress(placeEndPoint);
-        console.log(returnedAddress);
+        
+        getAddress(placeEndPoint, function(address) {
+            var restaurantAddress = $('<h4>').addClass('text-muted').append(address);
+            var addressContainer = $('<div>').addClass('col-xs-12').append(restaurantAddress);
+            var addressRow = $('<div>').addClass('row').append(addressContainer);
+            restaurantColumn.append(addressRow);
 
+        });
 
 
         //Append all columns to panel
@@ -66,8 +69,6 @@ $(document).ready(function() {
         var contentPanelBody = panelBody.append(contentRow);
         var newPanel = panel.append(contentPanelBody);
         var panelContents = $('.main-column').append(newPanel);
-
-
 
     }
 
