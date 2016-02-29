15d7f904677a71cbc21599bd33e8008ea16079fb
Matthew Christopher Taggart <mtaggart89@gmail.com>
2016-02-28 20:24:23 -0500

    Successfully addedd first and second columns using ajax call from google places api to table.

--- public/js/googlePlaces.js
+++ public/js/googlePlaces.js
@@ -9,32 +9,54 @@ $(document).ready(function() {
 
     //Build URL
     var apiEndPoint = googlePlacesURL + 'key=' + apiKey + '&location=' + latLng + '&radius=' + radius + '&type=' + type;
-
-    //Reveal Data
-    // $.getJSON(apiEndPoint, function(data) {
-    //     buildPanel(data);
-    // });
-
-    buildPanel()
+    console.log(apiEndPoint);
+    $.getJSON(apiEndPoint, function(data) {
+        buildPanel(data);
+    });
     //Build Panel w/ Restaurant Data
     function buildPanel(data) {
         var panel = $('<div>').addClass('panel panel-default');
         var panelBody = $('<div>').addClass('panel-body');
-        var row = $('<div>').addClass('row');
-        var col4 = $('<div>').addClass('col-xs-4');
+        // var col4 = $('<div>').addClass('col-xs-4');
         var col12 = $('<div>').addClass('col-xs-12');
         var image = $('<img>')
             .addClass('img-thumbnail')
             .attr('src', 'http://img2.10bestmedia.com/Images/Photos/261521/Restaurant---Cristal-Room--J-r-me-Mondi-re_54_990x660.jpg')
             .attr('alt', '...');
- 
-        var imgColumn = col4.append(image);
-        var imgRow = row.append(imgColumn);
-        var imgBody = panelBody.append(imgRow);
-        var imgContainer = panel.append(imgBody);
+        
+        //Dynamically add first column of restaurant data
+        var imgColumn = $('<div>').addClass('col-xs-4').append(image);
 
-        var panelContents = $('.main-column').append(imgContainer);
-        var newEntry = $('.main-row').append(panelContents);
+        //Dynamically add 2nd column of restaurant data
+        var restaurant = data.results[1].name;
+        var restaurantTitle = $('<h2>').html(restaurant);
+        var restaurantContainer = col12.append(restaurantTitle);
+        var restaurantRow = $('<div>').addClass('row').append(restaurantContainer);
+        var restaurantColumn = $('<div>').addClass('col-xs-4').append(restaurantRow);
+
+
+        // var placeURL = 'https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyCoy7UBpNXFlBQKUGDtNz0ZhkgYC2cpPkg&placeid='
+        // var placeId = data.results[0].place_id;
+        // var placeEndPoint = placeURL + placeId;
+        // $.getJSON(placeEndPoint, function(placeData) {
+        //     var streetNumber = data.results[0].addressComponents[0].short_name;
+        //     var streetName = data.results[0].addressComponents[1].short_name;
+        //     var city =  data.results[0].addressComponents[2].short_name;
+        //     var state = data.results[0].addressComponents[3].short_name;
+        //     var zipCode = data.results[0].addressComponents[4].short_name;
+        //     var address = streetNumber + " " + streetName + " " + city + " " + state + " " + zipCode; 
+
+        // });
+
+        //Append all columns to panel
+        var contentRow = $('<div>').addClass('row content-row');
+        var imageContainer = contentRow.append(imgColumn);
+        var restaurantContainer = contentRow.append(restaurantColumn);
+        var contentPanelBody = panelBody.append(contentRow);
+        var newPanel = panel.append(contentPanelBody);
+        var panelContents = $('.main-column').append(newPanel);
+
+
 
     }
 
