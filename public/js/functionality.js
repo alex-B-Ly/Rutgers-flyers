55a9eca3c72c970b18838f73ededbb99843337c2
Alex Ly <alexly@nbp-91-172.nbp.ruw.rutgers.edu>
2016-03-03 16:56:41 -0500

    fixed register validation bugs

--- public/js/functionality.js
+++ public/js/functionality.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
       if($('#reg-fname').val().length < 1 || $('#reg-lname').val().length < 1){
         $('.reg-error').text('You must enter a valid name');        
         nameFlag = false;
+        validCheck();
       }else{
         $('.reg-error').empty();
         nameFlag = true;
@@ -28,6 +29,7 @@ $(document).ready(function() {
       if(emailInput.length > 0 && (!emailInput.match(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i))){
         $('.reg-error').text('Please enter a valid email');
         emailFlag = false;
+        validCheck();
       }else if(emailInput.match(/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i)){
         $('.reg-error').empty();
         emailFlag = true;
@@ -40,8 +42,12 @@ $(document).ready(function() {
       var usernameInput = $('#reg-username').val();
       if(!usernameInput){
         $('.reg-error').text('You must choose a username.');
+        usernameFlag = false;
+        validCheck();
       }else if(usernameInput.length < 5){
         $('.reg-error').text('Your username must be at least 5 characters long.');
+        usernameFlag = false;
+        validCheck();
       }else{
         $('.reg-error').empty();
         usernameFlag = true;
@@ -57,6 +63,7 @@ $(document).ready(function() {
       if(rePass !== password){
         $('.reg-error').text('Your passwords don\'t match.');
         passwordFlag = false;
+        validCheck();
       }else{
         $('.reg-error').empty();
         passwordFlag = true;
